6fc9c5e3a7f43d21c751da0e00fb29eba1e67bf7
Stollpy <darrasflorian42@gmail.com>
2021-12-06 20:03:58 -0500

    add mongodb on all repository product

--- Repositories/ProductsRepository.cs
+++ Repositories/ProductsRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using FirstApiCS.Entity;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace FirstApiCS.Repositories
@@ -11,26 +12,22 @@ namespace FirstApiCS.Repositories
     public class ProductsRepository : IProductRepository
     {
         private const string databaseName = "firstApiCS";
+        
         private const string collectionName = "products";
         
         private readonly IMongoCollection<Product> productCollection;
+
+        private readonly FilterDefinitionBuilder<Product> filterBuilder = Builders<Product>.Filter;
         public ProductsRepository(IMongoClient mongoClient)
         {
             IMongoDatabase database = mongoClient.GetDatabase(databaseName);
             productCollection = database.GetCollection<Product>(collectionName);
 
         }
-        
-        private readonly List<Product> products = new()
-        {
-            new Product {Id = Guid.NewGuid(), Name = "Learning CS", Price = 9, Quantity = 10, CreatedAt = DateTimeOffset.UtcNow},
-            new Product {Id = Guid.NewGuid(), Name = "Learning PHP", Price = 9, Quantity = 10, CreatedAt = DateTimeOffset.UtcNow},
-            new Product {Id = Guid.NewGuid(), Name = "Learning JavaScript", Price = 9, Quantity = 10, CreatedAt = DateTimeOffset.UtcNow},
-        };
 
         public IEnumerable<Product> GetProducts()
         {
-            return products;
+            return productCollection.Find(new BsonDocument()).ToList();
         }
 
         public void SetProduct(Product product)
@@ -40,19 +37,20 @@ namespace FirstApiCS.Repositories
 
         public void UpdateProduct(Product product)
         {
-            var index = products.FindIndex(oldProduct => oldProduct.Id == product.Id);
-            products[index] = product;
+            var filter = filterBuilder.Eq(existingProduct => existingProduct.Id, product.Id);
+            productCollection.ReplaceOne(filter, product);
         }
 
         public void DeleteProduct(Guid id)
         {
-            var index = products.FindIndex(oldProduct => oldProduct.Id == id);
-            products.RemoveAt(index);
+            var filter = filterBuilder.Eq(product => product.Id, id);
+            productCollection.DeleteOne(filter);
         }
 
         public Product? GetProduct(Guid id)
         {
-            return products.Where(product => product.Id == id).SingleOrDefault();
+            var filter = filterBuilder.Eq(product => product.Id, id);
+            return productCollection.Find(filter).SingleOrDefault();
         }
     }
 }
