da144846d2605a69fecd9bfe73215f3095abed78
Stollpy <darrasflorian42@gmail.com>
2021-12-08 20:13:46 -0500

    add secret dotnet and auth mongo

--- Repositories/ProductsRepository.cs
+++ Repositories/ProductsRepository.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using FirstApiCS.Dtos.Product;
 using FirstApiCS.Entity;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
--- obj/Debug/net6.0/FirstApiCS.AssemblyInfo.cs
+++ obj/Debug/net6.0/FirstApiCS.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("cf6bc29d-f008-4d8e-a2a3-21208d79aaf6")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstApiCS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
